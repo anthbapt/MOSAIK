209f2a8c37025c711e706ff53a1d6795a0d431d6
Anthony Baptista <35119963+anthbapt@users.noreply.github.com>
2025-05-08 11:42:50 +0100

    Add files via upload

--- src/qc/cosmx_QC.py
+++ src/qc/cosmx_QC.py
@@ -1,3 +1,8 @@
+# =============================================================================
+# 
+# QuarterBrain
+#
+# =============================================================================
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
@@ -5,7 +10,7 @@ import warnings
 import logging
 logging.basicConfig(level=logging.WARNING)
 warnings.filterwarnings("ignore")
-path = '/Users/k2481276/Documents/CosMx/'
+path = r"/Volumes/Extreme SSD/QuarterBrain"
 os.chdir(path)
 
 from spatialdata.transformations import Affine, set_transformation
@@ -21,8 +26,8 @@ import cosmx
 # =============================================================================
 # Paths & Data Loading
 # =============================================================================
-slide = '/TMAGLACIER2'
-zarr_path = "TMAGLACIER2.zarr"
+zarr_path = "QuarterBrain.zarr"
+slide = "/flatFiles"
 
 flat_file_dir_slide = path + slide
 
@@ -32,24 +37,25 @@ metafile_df = pd.read_csv(flat_file_dir_slide+ '/' + metafile)
 fovfile = [item for item in os.listdir(flat_file_dir_slide) if 'fov_positions_file' in item][0]
 fovfile_df = pd.read_csv(flat_file_dir_slide + '/' + fovfile)
 
+polygon = [item for item in os.listdir(flat_file_dir_slide) if 'polygons' in item][0]
+polygon_df = pd.read_csv(flat_file_dir_slide + '/' + polygon)
+
 first_run = user_input = input("Is it the first run (0: False, 1: True): ")
 
 if first_run == '1':
-    columns_sub = ['fov','Area','AspectRatio','CenterX_local_px','CenterY_local_px','Width',
-    'Height','Mean.PanCK','Max.PanCK','Mean.G','Max.G','Mean.Membrane','Max.Membrane',
-    'Mean.CD45','Max.CD45','Mean.DAPI','Max.DAPI','cell_ID','assay_type','version',
-    'Run_Tissue_name','Panel','cellSegmentationSetId','cellSegmentationSetName',
-    'slide_ID','CenterX_global_px','CenterY_global_px','unassignedTranscripts']
-    
-    metafile_df = pd.read_csv(flat_file_dir_slide+ '/' + metafile)
-    metafile_df = metafile_df[columns_sub]
-    metafile_df.to_csv(path + slide + '/' + metafile, index = False)
     
     fovfile_df = pd.read_csv(flat_file_dir_slide + '/' + fovfile)
     fovfile_df = fovfile_df.rename({'FOV': 'fov'}, axis = 'columns')
     fovfile_df.to_csv(path + slide + '/' + fovfile, index = False)
     
-    sdata = cosmx.cosmx(path + slide, transcripts=True)
+    polygon_df = pd.read_csv(flat_file_dir_slide + '/' + polygon)
+    polygon_df = polygon_df.rename({'cellID': 'cell_ID'}, axis = 'columns')
+    if 'cell' not in polygon_df.keys():
+        polygon_df['cell'] = "c_" + polygon_df['fov'].astype(str) + '_' + polygon_df['cell_ID'].astype(str)
+    polygon_df.to_csv(path + slide + '/' + polygon, index = False)
+    
+        
+    sdata = cosmx.cosmx(path + slide, flip_image = True)
     sdata.write(zarr_path)    
 
 # =============================================================================
@@ -60,66 +66,13 @@ adata = sdata.tables["table"]
 print(adata.obs.keys())
 
 # FOV positions 
-# Should be defined manually with the help of the Napari visualisation
-sample1 = list(range(1, 5 + 1))
-sample2 = list(range(8, 16 + 1))
-sample3 = list(range(19, 27 + 1))
-sample4 = list(range(33, 37 + 1))
-sample5 = list(range(38, 43 + 1))
-sample6 = list(range(44, 52 + 1))
-sample7 = list(range(53, 61 + 1))
-sample8 = list(range(62, 67 + 1))
-sample9 = list(range(68, 76 + 1))
-sample10 = list(range(77, 85 + 1))
-sample11 = list(range(86, 92 + 1))
-sample12 = list(range(93, 101 + 1))
-sample13 = list(range(102, 110 + 1))
-sample14 = list(range(111, 119 + 1))
-sample15 = list(range(120, 128 + 1))
-sample16 = list(range(130, 133 + 1))
-sample17 = list(range(134, 137 + 1))
-sample18 = list(range(138, 143 + 1))
-sample19 = list(range(144, 152 + 1))
-sample20 = list(range(153, 161 + 1))
-sample21 = list(range(162, 170 + 1))
-sample22 = list(range(171, 179 + 1))
-sample23 = list(range(180, 188 + 1))
-sample24 = list(range(190, 198 + 1))
-sample25 = list(range(199, 207 + 1))
-sample26 = list(range(208, 213 + 1))
-sample27 = list(range(214, 222 + 1))
-sample28 = list(range(223, 231 + 1))
-sample29 = list(range(232, 240 + 1))
-sample30 = list(range(241, 249 + 1))
-sample31 = list(range(250, 258 + 1))
-sample32 = list(range(250, 258 + 1))
-sample33 = list(range(259, 267 + 1))
-sample34 = list(range(268, 276 + 1))
-sample35 = list(range(279, 282 + 1))
-sample36 = list(range(283, 288 + 1))
-sample37 = list(range(289, 297 + 1))
-sample38 = list(range(298, 306 + 1))
-sample39 = list(range(307, 315 + 1))
-sample40 = list(range(316, 324 + 1))
-sample41 = list(range(325, 333 + 1))
-sample42 = list(range(334, 342 + 1))
-sample43 = list(range(343, 351 + 1))
-sample44 = list(range(352, 360 + 1))
-sample45 = list(range(361, 369 + 1))
-sample46 = list(range(370, 378 + 1))
-sample47 = list(range(379, 387 + 1))
-sample48 = list(range(388, 396 + 1))
-sample49 = list(range(397, 405 + 1))
-sample50 = [6, 7, 17, 18, 28, 29, 30, 31, 32, 129, 189, 277, 278]
+# # Should be defined manually with the help of the Napari visualisation
+# TMAGLACIER1
+# =============================================================================
+sample1 = list(range(1, 66 + 1))
 
-samples_list = [sample1, sample2, sample3, sample4, sample5, sample6, sample7,\
-                sample8, sample9, sample10, sample11, sample12, sample13, sample14, \
-                sample15, sample16, sample17, sample18, sample19, sample20, sample21, \
-                sample22, sample23, sample24, sample25, sample26, sample27, sample28, \
-                sample29, sample30, sample31, sample32, sample33, sample34, sample35, \
-                sample36, sample37, sample38, sample39, sample40, sample41, sample42, \
-                sample43, sample44, sample45, sample46, sample47, sample48, sample49, \
-                sample50]
+samples_list = [sample1]
+    
 fovfile_df['sampleID'] = pd.Series()
 compt = 0
 for k in samples_list:
@@ -200,17 +153,6 @@ sns.histplot(adata.obs.groupby("fov")["total_counts"].sum(), kde=False, ax=axs[2
 plt.tight_layout(rect=[0, 0, 1, 0.99])
 plt.savefig('statistics_FOV.png', format = 'png', dpi = 600)
 
-fig, axs = plt.subplots(1, 3, figsize=(15, 4))
-fig.suptitle('Transcripts Statistics sampleID', fontsize=16)
-axs[0].set_title("Total transcripts per cell")
-sns.barplot(adata.obs.groupby("sampleID")["total_counts"].sum(), ax=axs[0])
-axs[1].set_title("Unique transcripts per cell")
-sns.barplot(adata.obs.groupby("sampleID")["n_genes_by_counts"].sum(), ax=axs[1])
-axs[2].set_title("Transcripts per FOV")
-sns.barplot(adata.obs.groupby("sampleID")["total_counts"].sum(), ax=axs[2])
-plt.tight_layout(rect=[0, 0, 1, 0.99])
-plt.savefig('statistics_sampleID.png', format = 'png', dpi = 600)
-
 # ========================
 # Filtering & Normalization
 # ========================
@@ -251,5 +193,5 @@ plt.savefig('Sample_display_transcripts_cluster.png', format = 'png', dpi = 600)
 # ========================
 # Visualise specific ROI
 # ========================
-fov =  '275'
-visualise_fov(sdata, coordinate = 'global')
+fov =  '32'
+visualise_fov(sdata, fov, coordinate = 'global')
